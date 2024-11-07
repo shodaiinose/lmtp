0751ec3da35b7ec9e7ced2da5a0cb1332a126183
Shodai <68819024+shodaiinose@users.noreply.github.com>
2024-11-07 12:39:34 -0500

    Update sl.R

--- R/sl.R
+++ R/sl.R
@@ -1,5 +1,5 @@
 run_ensemble <- function(data, y, weights, learners, outcome_type, id, folds, discrete, info) {
-  mlr3superlearner::mlr3superlearner(
+  mlr3superlearnerweights::mlr3superlearner(
     data = data,
     target = y,
     library = learners,
